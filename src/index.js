df1fb810ca4abddb4aa6315d2fb65ea5049e97f6
Jeremy D. Kincaid <jeremydkincaid@gmail.com>
2021-10-09 17:26:24 -0400

    fined tuned animations

--- src/index.js
+++ src/index.js
@@ -15,18 +15,24 @@ import {
     CameraHelper,
     PCFShadowMap,
     PCFSoftShadowMap,
-    Color
+    Color,
+    Vector3
 } from 'three';
 
-let isLosingWall = false;
-let isBringinEmBack = false;
+let vLeft = new Vector3(-25, 12.5, 0);
+let vRight = new Vector3(25, 12.5, 0);
+let vBack = new Vector3(0, 12.5, -25);
+let vFloor = new Vector3(0, -12.5, 0);
+
+let isMovingWall = false;
 const changeBtn = document.getElementById('change-color');
 changeBtn.addEventListener('click', patience);
-function patience(){
-    if(isLosingWall || isBringinEmBack) {
+function patience() {
+    if (isMovingWall) {
         alert("BE PATIENT YOU FOOL");
     } else {
-        isLosingWall = true;
+        isMovingWall = true;
+        vLeft.setX(-100.2);
     }
 }
 
@@ -39,118 +45,87 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = BasicShadowMap;
 document.body.appendChild(renderer.domElement);
 
-// const light = new AmbientLight( 0x404040 );
-// scene.add( light );
-
 const pLight = new PointLight(0x4287f5, 1, 0, 2);
-pLight.position.set(0, 15, -15);
+pLight.position.set(0, 15, 0);
 pLight.castShadow = true;
 scene.add(pLight);
-
-// const dLight = new DirectionalLight( 0x4287f5 , 1, 100);
-// dLight.castShadow = true;
-// dLight.position.set(0, 1, 0);
-
-// scene.add( dLight );
-
-// dLight.shadow.mapSize.width = 512; // default
-// dLight.shadow.mapSize.height = 512; // default
-// dLight.shadow.camera.near = 0.5; // default
-// dLight.shadow.camera.far = 500; // default
 
 const geometry = new ConeGeometry(5, 5, 4);
 let matParams = { color: 0xffffff };
 const material = new MeshStandardMaterial(matParams);
 
 const pyramid = new Mesh(geometry, material);
-pyramid.position.set(0, 0, -15);
+pyramid.position.set(0, -5, 0);
 pyramid.receiveShadow = false;
 pyramid.castShadow = true;
 scene.add(pyramid);
 
-const pGeo = new PlaneGeometry(50, 25, 2, 2);
-const pMaterial = new MeshStandardMaterial({ color: 0x4287f5, side: DoubleSide });
+const planeGeo = new PlaneGeometry(50, 50, 2, 2);
+const blueMaterial = new MeshStandardMaterial({ color: 0x4287f5, side: DoubleSide });
+const redMaterial = new MeshStandardMaterial({ color: 0xfa020b, side: DoubleSide });
 
 const floorCeiling = new PlaneGeometry(50, 50, 2, 2);
 
-const backWall = new Mesh(pGeo, pMaterial);
-backWall.position.set(0, 0, -25);
+const backWall = new Mesh(planeGeo, blueMaterial);
 backWall.receiveShadow = true;
 scene.add(backWall);
 
-const leftWall = new Mesh(pGeo, pMaterial);
-leftWall.position.set(-25, 0, 0);
+const leftWall = new Mesh(planeGeo, blueMaterial);
 leftWall.rotation.set(0, 1.5708, 0);
 scene.add(leftWall);
 
-const rightWall = new Mesh(pGeo, pMaterial);
-rightWall.position.set(25, 0, 0);
+const rightWall = new Mesh(planeGeo, blueMaterial);
 rightWall.rotation.set(0, 1.5708, 0);
 scene.add(rightWall);
 
-const floor = new Mesh(floorCeiling, pMaterial);
-floor.position.set(0, -12.5, 0);
+const floor = new Mesh(floorCeiling, blueMaterial);
 floor.rotation.set(1.5708, 0, 0);
 floor.receiveShadow = true;
 scene.add(floor);
 
-function loseWalls() {
-
-    if(leftWall.position.x >= -100.0) {
-        if (leftWall.position.x >= -30.0) {
-            leftWall.position.x -= 0.5;
-            rightWall.position.x += 0.5;
-            backWall.position.y += 0.5;
-            floor.position.y -= 0.5;
-        } else {
-            leftWall.position.x -= 1.0;
-            rightWall.position.x += 1.0;
-            backWall.position.y += 1.0;
-            floor.position.y -= 1.0;
-        }    
-    } else {
-        let blue = new Color(0x4287f5);
-        pMaterial.color.equals(blue) ? pMaterial.color.setHex(0xfa020b) : pMaterial.color.setHex(0x4287f5);
-        isLosingWall = false;    
-        isBringinEmBack = true;
-    }
-
-}
-
-/**
- * bring those walls back;
- */
-function bringEmBack() {
-    let leftWallX = leftWall.position.x;
-    if(leftWallX < -25.0) {
-        if (leftWallX < -30.0) {
-            leftWall.position.x += 1.0;
-            rightWall.position.x -= 1.0;
-            backWall.position.y -= 1.0;
-            floor.position.y += 1.0;
-        } else {
-            leftWall.position.x += 0.5;
-            rightWall.position.x -= 0.5;
-            backWall.position.y -= 0.5;
-            floor.position.y += 0.5;
-        }
-    } else {
-        isBringinEmBack = false;
-    }
-}
-
-camera.position.z = 50;
+camera.position.z = 20;
+camera.position.y = 15;
+camera.rotation.x = -0.523599;
 
 function animate() {
     requestAnimationFrame(animate);
-    console.log(leftWall.position.x);
-    if(isLosingWall) {
-        loseWalls();
+
+    if(leftWall.position.x <= -100.0) {
+        vLeft.setX(-25.0);
+        vRight.setX(100.2);
+
+        leftWall.material == blueMaterial ? leftWall.material = redMaterial : leftWall.material = blueMaterial;
     }
-    if(isBringinEmBack) {
-        bringEmBack();
+
+    if(rightWall.position.x >= 100.0) {
+        vRight.setX(25.0);
+        vBack.setY(87.7);
+
+        rightWall.material == blueMaterial ? rightWall.material = redMaterial : rightWall.material = blueMaterial;
     }
+
+    if(backWall.position.y >= 87.5) {
+        vBack.setY(12.5);
+        vFloor.setY(-87.7);
+
+        backWall.material == blueMaterial ? backWall.material = redMaterial : backWall.material = blueMaterial;
+    }
+
+    if(floor.position.y <= -87.5) {
+        vFloor.setY(-12.5);
+
+        floor.material == blueMaterial ? floor.material = redMaterial : floor.material = blueMaterial;
+        pLight.color.equals(new Color(0x4287f5)) ? pLight.color.set(0xfa020b) : pLight.color.set(0x4287f5);
+        isMovingWall = false;
+    }
+
+    leftWall.position.lerp(vLeft, 0.3);
+    rightWall.position.lerp(vRight, 0.3);
+    backWall.position.lerp(vBack, 0.3);
+    floor.position.lerp(vFloor, 0.3);
+
     pyramid.rotation.y += 0.01;
+
     renderer.render(scene, camera);
 }
 animate();
